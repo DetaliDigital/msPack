e64c32cffd4a16da78fb4f5f4e6f94f3103a540d
Marabar <marat@marabar.ru>
2016-11-10 15:38:45 +0500

    Удалена проверка на загрузку страницы

--- assets/components/mspack/js/web/mspack.js
+++ assets/components/mspack/js/web/mspack.js
@@ -1,10 +1,9 @@
 var msPack = {
     initialize: function(config)
     {
-        $(window).load(function(){
-            var total = $('#msCart .ms2_total_cost').text();
-            $('#mspack-total').text(total);
-        });
+        var total = $('#msCart .ms2_total_cost').text();
+        $('#mspack-total').text(total);
+
         // После загрузки страницы получить стоимость заказа
         miniShop2.Callbacks.add('Order.getcost.response.success', 'orders_add', function(response) {
             var orderCost = response.data.cost; // Здесь всегда актуальная стоимость заказа без упаковки
